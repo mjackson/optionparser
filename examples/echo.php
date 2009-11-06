2958aed5cd8e1cba835bc2becbc18075c07e5ae7
Michael J. I. Jackson <mjijackson@gmail.com>
2009-11-06 12:05:34 -0700

    Added echo example

---
+++ examples/echo.php
@@ -0,0 +1,31 @@
+<?php
+
+require dirname(__FILE__) . '/../lib/OptionParser.php';
+
+$parser = new OptionParser;
+$parser->addHead("Echo the parameters of each flag as determined by OptionParser.\nUsage: " . basename($argv[0]) . ' [ options ]');
+$parser->addRule('a', "A short flag with no parameter");
+$parser->addRule('b:', "A short flag with an optional parameter");
+$parser->addRule('c::', "A short flag with a required parameter");
+$parser->addRule('long-a', "A long flag with no parameter");
+$parser->addRule('long-b:', "A long flag with an optional parameter");
+$parser->addRule('long-c::', "A long flag with a required parameter");
+
+$args = $argv;
+
+try {
+    $parser->parse($args);
+} catch (Exception $e) {
+    die($parser->getUsage());
+}
+
+$flagNames = array('a', 'b', 'c', 'long-a', 'long-b', 'long-c');
+
+echo "Parsed arguments:\n";
+foreach ($flagNames as $flag) {
+    $param = var_export($parser->getOption($flag), true);
+    echo sprintf(' %6s %s', $flag, $param) . "\n";
+}
+
+echo "\nRemaining arguments: " . implode(' ', $args) . "\n";
+
